5e090ed8a8cca73ff5081cdfff289217040e83d6
Borgnia <borgniaf@amazon.com>
2016-05-03 13:52:38 +0100

    new recipies

---
+++ deploy/metadata.rb
@@ -0,0 +1,9 @@
+name             'deploy'
+maintainer       'AMZ'
+maintainer_email 'borgniaf@amazon.com'
+license          'All rights reserved'
+description      'Installs/Configures deploy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apt"
